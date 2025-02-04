f6a397b2005582f1a27fa2ef2ceb46ba4e0257d5
AldairDo <aldair30d@gmail.com>
2025-02-04 05:56:57 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://AldairDominguez.github.io/MiBio',
   plugins: [vue()],
 })
